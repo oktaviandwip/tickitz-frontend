db72253a84e09b7803421d5061d0f0226e7ff40d
Oktavian Dwi Putra <oktavian.dwiputra@gmail.com>
2024-06-26 14:14:04 +0700

    Change name components folder

--- src/components/Home/Advantage.jsx
+++ src/components/Home/Advantage.jsx
@@ -2,7 +2,7 @@ const Advantage = ({ img, title, desc }) => {
   return (
     <>
       <div className="w-[327px] md:w-[230px] lg:w-[340px] flex flex-col items-center md:items-start text-center md:text-start mb-[33px] md:mb-0">
-        <img src={img} alt="guaranteed" />
+        <img src={img} alt="Advantage" />
         <div className="text-lg font-semibold leading-6 my-[19px]">{title}</div>
         <div className="text-dark-grey leading-8 tracking-[0.75px]">{desc}</div>
       </div>
